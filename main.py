aaa45dfe569ab2029f6ae2a460f6a070e54aa9cd
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-08-03 19:14:24 -0400

    Added point totalling for awards

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import request
 
+def countAwards(teams):
+    for team in teams:
+        awards = team["awards"]
+        if (awards):
+            for award in awards:
+                if award["type"] == "Winner" or award["type"] == "Finalist":
+                    continue
+                elif award["type"] == "Inspire":
+                    team["points"] += (2**(3 - award["placement"]) * 15)
+                else:
+                    team["points"] += (2**(3 - award["placement"]) * 3)
+
 def main():
-    print(request.getTeamsFromEvent("FTCCMP1FRAN"))
+    teams = request.getTeamsFromEvent("FTCCMP1FRAN")
+    
+    countAwards(teams)
+
+    for team in teams:
+        print(str(team["teamNumber"]) + ": " + str(team["points"]))
 
 
 if __name__ == "__main__":

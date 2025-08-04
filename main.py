00f3208ff2832e637374989ac307d8a11cf5cd59
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-08-04 18:16:01 -0400

    Implemented new ranking points calculation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import streamlit
 import sys
 import math
+from scipy.special import erfinv
+
 import request
 import eightalliance
 import sixalliance
@@ -19,10 +22,19 @@ def countAwards(teams):
                     team["points"] += (2**(3 - award["placement"]) * 3)
 
 def countRanking(teams):
-    step = 14 / (len(teams) - 1)
+    # step = 14 / (len(teams) - 1)
     for team in teams:
-        inverted_rank = (len(teams) - team["stats"]["rank"])
-        team["points"] += math.floor(inverted_rank * step) + 2
+        # inverted_rank = (len(teams) - team["stats"]["rank"])
+        # team["points"] += math.floor(inverted_rank * step) + 2
+
+        n = len(teams)
+        r = team["stats"]["rank"]
+        alpha = 1.07
+
+        # Standard formala given by competition manual
+        qual_points = erfinv((n - 2*r + 2) / (alpha*n)) * 7 / erfinv(1 / alpha) + 9
+
+        team["points"] += math.floor(qual_points)
 
 def countAdvancement(teams):
     if len(teams) > 40:
@@ -58,8 +70,9 @@ def main():
     countAlliance(teams)
 
     teams.sort(key=lambda team: team["points"], reverse=True)
+
     for team in teams:
-        print(str(team["teamNumber"]) + ": " + str(team["points"]))
+        streamlit.write(str(team["teamNumber"]) + ": " + str(team["points"]))
 
 
 if __name__ == "__main__":
